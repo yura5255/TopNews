deb8c50c0dce166dcd04fc364658c9955fdcdbd7
Yura Bortnik <Bort_lx45@student.itstep.org>
2023-07-10 19:05:26 +0300

    Start with dashboard layout

---
+++ TopNews/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TopNews.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
