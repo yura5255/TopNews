54884d6e813f1c917398be5e3de16bc48343ce21
Yura Bortnik <Bort_lx45@student.itstep.org>
2023-07-12 19:26:59 +0300

    Add login validation

---
+++ TopNews.Core/Validation/User/LoginUserValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopNews.Core.DTOs.User;
+
+namespace TopNews.Core.Validation.User
+{
+    public class LoginUserValidation : AbstractValidator<DTOUserLogin>
+    {
+        public LoginUserValidation()
+        {
+            RuleFor(r => r.Email).NotEmpty().WithMessage("Field must not be empty!").EmailAddress().WithMessage("Invalid email format!");
+            RuleFor(r => r.Password).NotEmpty().WithMessage("Field must not be empty!").MinimumLength(6).WithMessage("Password must at least 6 characters!").MaximumLength(128);
+        }
+    }
+}
--- TopNews/Controllers/DashboardController.cs
+++ TopNews/Controllers/DashboardController.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TopNews.Core.DTOs.User;
+using TopNews.Core.Validation.User;
 
 namespace TopNews.Web.Controllers
 {
+    [Authorize]
     public class DashboardController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+        [AllowAnonymous] //GET
         public IActionResult Login()
         {
             return View();
+        }
+        [AllowAnonymous] //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(DTOUserLogin model)
+        {
+            var validator = new LoginUserValidation();
+            var validationResult = validator.Validate(model);
+            if (validationResult.IsValid) 
+            {
+                return View(model);
+            }
+            ViewBag.AuthError = validationResult.Errors[0];
+            return View(model);
         }
     }
 }
