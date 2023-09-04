6f50b75e5e94a4be22bb576889beec64c6bf43dc
Yura Bortnik <Bort_lx45@student.itstep.org>
2023-09-04 20:35:35 +0300

    Main page for posts and preview

--- TopNews/Controllers/HomeController.cs
+++ TopNews/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TopNews.Core.DTOs.Post;
+using TopNews.Core.Interfaces;
 using TopNews.Models;
+using X.PagedList;
 
 namespace TopNews.Controllers
 {
     public class HomeController : Controller
     {
 
-        public IActionResult Index()
+        private readonly IPostService _postService;
+        private readonly ICategoryService _categoryService;
+        public HomeController(IPostService postService, ICategoryService categoryService)
         {
-            return View();
+            _categoryService = categoryService;
+            _postService = postService;
         }
+
+        public async Task<IActionResult> Index(int? page)
+        {
+            List<PostDto> posts = (await _postService.GetAll()).OrderByDescending(p => p.Id).ToList();
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View("Index", posts.ToPagedList(pageNumber, pageSize));
+        }
+
+
         [Route("Error/{statusCode}")]
         public IActionResult Error(int statusCode)
         {
@@ -21,6 +37,26 @@ namespace TopNews.Controllers
                 default:
                     return View("Error");
             }
+        }
+        public async Task<IActionResult> ReadMore(int? id)
+        {
+            PostDto? post = await _postService.Get(id ?? 0);
+            if (post == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            post.CategoryName = (await _categoryService.Get(post.CategoryId)).Name;
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Search([FromForm] string searchString)
+        {
+            List<PostDto> posts = await _postService.Search(searchString);
+            int pageSize = 20;
+            int pageNumber = 1;
+            return View("Index", posts.ToPagedList(pageNumber, pageSize));
         }
     }
 }
