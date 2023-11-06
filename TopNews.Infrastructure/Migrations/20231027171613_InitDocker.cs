1caa023405dc99d1c13cc76f6314ab2d38df505f
Yura Bortnik <Bort_lx45@student.itstep.org>
2023-11-06 20:12:38 +0200

    Lite update

---
+++ TopNews.Infrastructure/Migrations/20231027171613_InitDocker.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopNews.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDocker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DashdoardAccesses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IpAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DashdoardAccesses", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DashdoardAccesses",
+                columns: new[] { "Id", "IpAddress" },
+                values: new object[] { 1, "0.0.0.0" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DashdoardAccesses");
+        }
+    }
+}
